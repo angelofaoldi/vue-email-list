6b78b6c276f0abc07808b26eb925bbe0c333b3db
angelofaoldi <faoldiangelo@gmail.com>
2024-05-28 17:13:14 +0200

    [ADD] function generation random email domains

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ AXIOS
 https://axios-http.com/docs/intro 
 
 */
+
+// FUNZIONE PER GENERARE MAIL CASUALE
+
+function casualMail() {
+    const dominio = ['gmail.com', 'yahoo.com', 'outlook.com'];
+    const dominioRandom = dominio[Math.floor(Math.random() * dominio.lenght)];
+    const account = Math.random().toString(36).substring(7); // Genera una stringa casuale
+    return `${account}@${dominioRandom}`;
+}
+
+console.log(casualMail);
+
+
